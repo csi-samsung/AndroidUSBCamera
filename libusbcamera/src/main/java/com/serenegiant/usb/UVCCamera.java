cbaec778af83a03370acaffadbf6b6f4290fad20
csi-samsung <sam_sung@csi-sensor.com.tw>
2021-09-24 13:37:48 +0800

    Update UVCCamera.java

--- libusbcamera/src/main/java/com/serenegiant/usb/UVCCamera.java
+++ libusbcamera/src/main/java/com/serenegiant/usb/UVCCamera.java
@@ -220,6 +220,7 @@ public class UVCCamera {
     	if (mNativePtr != 0 && TextUtils.isEmpty(mSupportedSize)) {
     		mSupportedSize = nativeGetSupportedSize(mNativePtr);
     	}
+		if (DEBUG) Log.v(TAG, "mSupportedSize:" + mSupportedSize);
 		nativeSetPreviewSize(mNativePtr, DEFAULT_PREVIEW_WIDTH, DEFAULT_PREVIEW_HEIGHT,
 			DEFAULT_PREVIEW_MIN_FPS, DEFAULT_PREVIEW_MAX_FPS, DEFAULT_PREVIEW_MODE, DEFAULT_BANDWIDTH);
     }
