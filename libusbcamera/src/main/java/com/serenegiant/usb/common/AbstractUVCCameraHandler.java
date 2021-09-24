4e60938c3d30c1acc7c4be004cd9e2273d16575d
csi-samsung <sam_sung@csi-sensor.com.tw>
2021-09-24 13:13:29 +0800

    Update AbstractUVCCameraHandler.java

--- libusbcamera/src/main/java/com/serenegiant/usb/common/AbstractUVCCameraHandler.java
+++ libusbcamera/src/main/java/com/serenegiant/usb/common/AbstractUVCCameraHandler.java
@@ -547,7 +547,7 @@ public abstract class AbstractUVCCameraHandler extends Handler {
             if ((mUVCCamera == null) || mIsPreviewing) return;
             try {
                 if (DEBUG) Log.v(TAG_THREAD, "setPreviewSize:"+mWidth+","+mHeight+","+mPreviewMode);
-                mUVCCamera.setPreviewSize(mWidth, mHeight, 30, 30, mPreviewMode, mBandwidthFactor);
+                mUVCCamera.setPreviewSize(mWidth, mHeight, 1, 30, mPreviewMode, mBandwidthFactor);
                 // 获取USB Camera预览数据，使用NV21颜色会失真
                 // 无论使用YUV还是MPEG，setFrameCallback的设置效果一致
 //				mUVCCamera.setFrameCallback(mIFrameCallback, UVCCamera.PIXEL_FORMAT_NV21);
@@ -559,8 +559,7 @@ public abstract class AbstractUVCCameraHandler extends Handler {
                     // fallback to YUV mode
                     if (DEBUG) Log.v(TAG_THREAD, "handleStartPreview fallback to YUV mode");
                     if (DEBUG) Log.v(TAG_THREAD, "setPreviewSize:"+mWidth+","+mHeight+","+UVCCamera.DEFAULT_PREVIEW_MODE+","+mBandwidthFactor);
-                    //mUVCCamera.setPreviewSize(mWidth, mHeight, 30, 30, UVCCamera.DEFAULT_PREVIEW_MODE, mBandwidthFactor);
-                    mUVCCamera.setPreviewSize(mWidth, mHeight, 30, 30, mPreviewMode, mBandwidthFactor);
+                    mUVCCamera.setPreviewSize(mWidth, mHeight, 1, 30, UVCCamera.DEFAULT_PREVIEW_MODE, mBandwidthFactor);
                     mUVCCamera.setFrameCallback(mIFrameCallback, UVCCamera.PIXEL_FORMAT_YUV);
                     if (DEBUG) Log.v(TAG_THREAD, "handleStartPreview ok");
                 } catch (final IllegalArgumentException e1) {
@@ -829,6 +828,9 @@ public abstract class AbstractUVCCameraHandler extends Handler {
                 int len = frame.capacity();
                 final byte[] yuv = new byte[len];
                 frame.get(yuv);
+
+                if (DEBUG) Log.v(TAG_THREAD, mPreviewListener+" onFrame: " + yuv.length);
+
                 // nv21 yuv data callback
                 if (mPreviewListener != null) {
                     mPreviewListener.onPreviewResult(yuv);
